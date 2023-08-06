6f3c0c63d5a927ed2b1fb1c8b22b40adfe097d7d
ooziava <oleg.mikros@gmail.com>
2023-08-07 01:27:26 +0300

    spotify searching improved

--- services/socials/spotify.ts
+++ services/socials/spotify.ts
@@ -6,21 +6,27 @@ export default async (query: string): Promise<Song[]> => {
 
   if (res.type === "playlist") {
     const tracks = await (res as SpotifyPlaylist).all_tracks();
-    const songs = tracks.map((track) => ({
-      url: track.url,
-      title: track.name,
-      thumbnail: track.thumbnail?.url ?? "",
-      duration: track.durationInSec,
-      playlist: res.name,
-      timestamp: new Date(),
-    }));
 
     return Promise.all(
-      songs.map(async (song) => {
-        const newSong = await search(song.title, { limit: 1 });
-        song.url = newSong[0].url;
-        song.duration = newSong[0].durationInSec;
-        return song;
+      tracks.map(async (song) => {
+        const [newSong] = await search(
+          `${song.name} ${song.artists.map((artst) => artst.name).join(" ")}`,
+          { limit: 1 }
+        );
+
+        return {
+          url: newSong.url,
+          title: newSong.title || song.name,
+          thumbnail: newSong.thumbnails[0].url,
+          duration: newSong.durationInSec,
+          playlist: res.name,
+          author: {
+            name: newSong.channel?.name || song.artists[0].name,
+            url: newSong.channel?.url || song.artists[0].url,
+            avatar: newSong.channel?.iconURL() || song.artists[0].url,
+          },
+          timestamp: new Date(),
+        };
       })
     );
   } else if (res.type === "track") {
