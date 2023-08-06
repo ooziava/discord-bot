871c45e961cf288f3f180078fe701c321f742bed
ooziava <oleg.mikros@gmail.com>
2023-08-06 17:42:16 +0300

    express removed

--- index.ts
+++ index.ts
@@ -1,65 +1,48 @@
-import express, { Request, Response } from "express";
-import "./bot.js";
-const app = express();
-const port = process.env.PORT || 3001;
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
 
-app.get("/", (req: Request, res: Response) => res.type("html").send(html));
+import { type Bot, type Command } from "interfaces/discordjs";
+import loadCommands from "./utils/loadCommands.js";
+import registerCommands from "./utils/registerCommands.js";
+import commandHandler from "./utils/commandHandlers.js";
+import socialAuth from "./utils/socialAuth.js";
 
-const server = app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
+dotenv.config();
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildEmojisAndStickers,
+  ],
+});
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`;
-export default server;
+let commands = new Collection<string, Command>();
+
+client.once(Events.ClientReady, async (c) => {
+  await socialAuth();
+  commands = await loadCommands();
+  bot.commands = commands;
+
+  c.on(Events.InteractionCreate, (interaction) => {
+    commandHandler(interaction, bot);
+  });
+
+  await registerCommands(commands);
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+const bot: Bot = {
+  client,
+  commands,
+  activeMessageIds: new Collection(),
+  songAttributes: new Collection(),
+  subscriptions: new Collection(),
+  interactions: new Collection(),
+  currentSong: new Collection(),
+};
+
+client.login(process.env.DISCORD_TOKEN);
+export default bot;
