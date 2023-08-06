bba9122c1e0e45f1f41711364dc0b22e755fccd6
ooziava <oleg.mikros@gmail.com>
2023-08-06 18:07:29 +0300

    port bug fixed

--- index.ts
+++ index.ts
@@ -49,12 +49,11 @@ client.login(process.env.DISCORD_TOKEN);
 export default bot;
 
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
