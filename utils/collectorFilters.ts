59f6a5355e77c1896ea5e5bb334a072e0db22f60
ooziava <oleg.mikros@gmail.com>
2023-09-30 19:15:10 +0300

    new init

--- utils/collectorFilters.ts
+++
@@ -1,9 +0,0 @@
-import { CommandInteraction, Interaction } from "discord.js";
-
-type Filter = (i: Interaction) => boolean;
-
-const userFilter = (interaction: CommandInteraction): Filter => {
-  return (i: Interaction): boolean => i.user.id === interaction.user.id;
-};
-
-export { userFilter };
