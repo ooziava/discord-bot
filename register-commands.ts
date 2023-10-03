4f9605284d81a47c819015ec61d45ce5d337f00a
ooziava <oleg.mikros@gmail.com>
2023-10-03 21:58:38 +0300

    npm fix

--- register-commands.ts
+++ register-commands.ts
@@ -4,9 +4,20 @@ import { REST, Routes } from "discord.js";
 const rest = new REST().setToken(process.env.TOKEN!);
 
 export default async (commandCollection: Command[]) => {
+  await rest.put(
+    process.env.NODE_ENV === "development"
+      ? Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.SERVER_ID!)
+      : Routes.applicationCommands(process.env.CLIENT_ID!),
+    { body: [] }
+  );
   if (!commandCollection.length) return;
-
-  const commands = commandCollection.map((commandItem) => commandItem.data.toJSON());
+  const commands =
+    process.env.NODE_ENV === "development"
+      ? commandCollection.map((commandItem) => {
+          commandItem.data.name += "_dev";
+          return commandItem.data.toJSON();
+        })
+      : commandCollection.map((commandItem) => commandItem.data.toJSON());
 
   try {
     const data = (await rest.put(
